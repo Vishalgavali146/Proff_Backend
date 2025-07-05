a5cf4a904f0c58db662614a11360e2c3c3b47e1f
Vishalgavali146 <gavalivishal146@gmail.com>
2025-07-05 23:50:56 +0530

    fix refreshAccessToken logic and add subscription model

---
+++ src/models/subcription.models.js
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+    suscriber: {
+    type: Schema.Types.ObjectId,//one who is subscribing
+    ref: "User"
+},
+     channel: {
+        type: Schema.Types.ObjectId, //one whom 'subscribed is suscibing
+        ref: "User"
+     },
+     
+},{
+    timestamps: true,
+})
+
+
+
+
+export const subscription = mongoose.model("subscription", subscriptionSchema);
